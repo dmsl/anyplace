ae37adf585d4a758dee8ce67a250afe91fdac392
Gibran Felix <gibranfelix@gmail.com>
2016-04-14 16:25:03 -0500

    Anyplace Android
    Anyplace Android was migrated to android studio

---
+++ android/library/build/generated/source/buildConfig/release/com/google/maps/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.maps.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.google.maps.android";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
