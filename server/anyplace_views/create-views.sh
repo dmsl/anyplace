0785f2a55f246d07d4a2ff4669da074cbaa8cd3e
costantinos <costa.costantinos@gmail.com>
2017-07-20 20:03:29 +0300

    Update the script in order to choose the bucket name through the variables

--- server/anyplace_views/create-views.sh
+++ server/anyplace_views/create-views.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 USERNAME=""
 PASSWORD=""
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_accounts -d @accounts.json -u $USERNAME:$PASSWORD
+BUCKET=""
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_accounts -d @accounts.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_admin -d @admin.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_admin -d @admin.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_campus -d @accounts.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_campus -d @accounts.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_floor -d @floor.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_floor -d @floor.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_magnetic -d @magnetic.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_magnetic -d @magnetic.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_nav -d @nav.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_nav -d @nav.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_radio -d @radio.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_radio -d @radio.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_radio_spatial -d @spatial_radio.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_radio_spatial -d @spatial_radio.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/dev_nav_spatial -d @spatial_nav.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/dev_nav_spatial -d @spatial_nav.json -u $USERNAME:$PASSWORD
 
 ##########
 # PUBLISH
 ##########
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/accounts -d @accounts.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/accounts -d @accounts.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/admin -d @admin.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/admin -d @admin.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/campus -d @accounts.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/campus -d @accounts.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/floor -d @floor.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/floor -d @floor.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/magnetic -d @magnetic.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/magnetic -d @magnetic.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/nav -d @nav.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/nav -d @nav.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/radio -d @radio.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/radio -d @radio.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/radio_spatial -d @spatial_radio.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/radio_spatial -d @spatial_radio.json -u $USERNAME:$PASSWORD
 
-curl -X PUT -H "Content-Type: application/json" http://localhost:8092/anyplace/_design/nav_spatial -d @spatial_nav.json -u $USERNAME:$PASSWORD
+curl -X PUT -H "Content-Type: application/json" http://localhost:8092/$BUCKET/_design/nav_spatial -d @spatial_nav.json -u $USERNAME:$PASSWORD
