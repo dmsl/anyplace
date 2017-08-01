bf680d6161e6cb68fb4c2c652a9474ca918d07e7
costantinos <costa.costantinos@gmail.com>
2017-08-01 17:26:25 +0300

    Removed the directories of the applications
    /android
    /ios
    /windows

--- android/library/src/com/google/maps/android/ui/BubbleIconFactory.java
+++
@@ -1,8 +0,0 @@
-package com.google.maps.android.ui;
-
-/**
- * Use {@link IconGenerator} instead.
- */
-@Deprecated
-public class BubbleIconFactory {
-}
--- ios/plugins/com.dmsl.anyplace.magloc/src/ios/APMagloc-Bridging-Header.h
+++
@@ -1,3 +0,0 @@
-
-#import <Cordova/CDV.h>
-#import "Localization/MCL Bridge/MCL-Bridging-Header.h"
