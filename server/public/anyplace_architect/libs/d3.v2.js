54e62770e9e246f31cb13ff032d47e6a98cc2010
Stefan Weil <sw@weilnetz.de>
2018-10-14 21:27:05 +0200

    anyplace_architect: Fix wrong declaration of local variable
    This fixes a warning from LGTM:
    
        Variable 'n' is used before its declaration.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- server/public/anyplace_architect/libs/d3.v2.js
+++ server/public/anyplace_architect/libs/d3.v2.js
@@ -247,9 +247,9 @@ d3.transpose = function(matrix) {
   return d3.zip.apply(d3, matrix);
 };
 d3.zip = function() {
-  if (!(n = arguments.length)) return [];
+  if (!(var n = arguments.length)) return [];
   for (var i = -1, m = d3.min(arguments, d3_zipLength), zips = new Array(m); ++i < m;) {
-    for (var j = -1, n, zip = zips[i] = new Array(n); ++j < n;) {
+    for (var j = -1, zip = zips[i] = new Array(n); ++j < n;) {
       zip[j] = arguments[j][i];
     }
   }
