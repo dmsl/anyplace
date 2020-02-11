b7e29a0b18ab302cc9e9cd782d5855358e7a3b35
Mike Karam <mike95gr@hotmail.com>
2020-02-11 16:51:18 +0200

    add new .gitignore file based on gitignore.io. change Docker files to play with the host network

--- library/anyplace-ros/.docker/docker_run_roscore.sh
+++ library/anyplace-ros/.docker/docker_run_roscore.sh
@@ -5,9 +5,10 @@ docker network create ros
 
 docker run --rm -it \
     --volume="$(dirname `pwd`)":/root/rosjava/src/anyplace-ros \
-    --net ros \
-    --name master \
+    --net host \
     ros-kinetic-rosjava \
     roscore
 
 docker network rm ros
+# --cap-add=NET_ADMIN \
+#     --name master \
--- library/anyplace-ros/.docker/docker_run_tester_1.sh
+++ library/anyplace-ros/.docker/docker_run_tester_1.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 
-docker network create ros
+# docker network create ros
 
 docker run --rm -it \
-    --net ros \
-    --name tester_1 \
+    --net host \
+    --cap-add=NET_ADMIN \
     --volume="$(dirname `pwd`)":/root/rosjava/src/anyplace-ros \
-    --env ROS_HOSTNAME=tester_1 \
-    --env ROS_MASTER_URI=http://master:11311 \
     ros-kinetic-rosjava \
     bash
 
-docker network rm ros
+# docker network rm ros
+# --net ros \
+# --name tester_1 \
+# --env ROS_HOSTNAME=tester_1 \
+# --env ROS_MASTER_URI=http://master:11311 \
