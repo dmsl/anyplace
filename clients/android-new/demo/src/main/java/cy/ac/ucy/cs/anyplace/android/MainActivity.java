0800cadad1ebea9fa65fbddbc10c0f9654bec3a5
cachil01 <cachil01@cs.ucy.ac.cy>
2020-07-28 13:47:44 +0300

    Basic project dependencies

--- clients/android-new/demo/src/main/java/cy/ac/ucy/cs/anyplace/android/MainActivity.java
+++ clients/android-new/demo/src/main/java/cy/ac/ucy/cs/anyplace/android/MainActivity.java
@@ -3,11 +3,16 @@ package cy.ac.ucy.cs.anyplace.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cy.ac.ucy.cs.anyplace.core.Anyplace;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Anyplace client;
+
   }
 }
