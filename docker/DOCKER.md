9d90a930b740600447a08d167b059dd06a33b1ec
Paschalis Mpeis <paschalis.mpeis@gmail.com>
2020-10-22 21:19:02 +0300

    Adding some missing files for docker, some bugfixes, and configuration tuning.
    anyplace docker was tested on a Catalina macbook with 3GB of RAM allocated for it.

---
+++ docker/DOCKER.md
@@ -0,0 +1,21 @@
+# ![Docker](https://t.ly/O7m8 "Docker") Docker installation
+
+Please follow instructions from the official docker website:
+[docs.docker.com/get-docker](https://docs.docker.com/get-docker/)
+
+# Useful commands:
+
+###Convenience script:
+```
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+```
+
+### Docker without sudo
+```
+sudo usermod -aG docker $USER
+```
+
+### 2. docker-compose
+Follow instructions here:
+https://docs.docker.com/compose/install/
--- docker/post_install.sh
+++ docker/post_install.sh
@@ -18,3 +18,24 @@ cwd=$(pwd)
 cd couchbase/anyplace_views
 ./create-views.sh
 cd $cwd
+
+echo -e "\n\n"
+echo "+------------------------------------------------------------"
+echo "| Post install configuration finished!"
+echo "| Make sure that no errors have occured."
+echo "| "
+echo "| NOTE: If you are using the custom generated SSL certificate"
+echo "|       please 'trust it' through your operating system!"
+echo "| "
+echo "| Couchbase will be available at:"
+echo "| $PLAY_SERVER_PROTOCOL$IP:$PORT"
+echo "| "
+echo "| Anyplace will be available at:"
+echo "| $PLAY_SERVER_PROTOCOL$IP/"
+echo "| $PLAY_SERVER_PROTOCOL$IP/viewer"
+echo "| $PLAY_SERVER_PROTOCOL$IP/architect"
+echo "| "
+echo "| "
+echo "| Thanks for using anyplace!"
+echo "| https://github.com/dmsl/anyplace"
+echo "+------------------------------------------------------------"
