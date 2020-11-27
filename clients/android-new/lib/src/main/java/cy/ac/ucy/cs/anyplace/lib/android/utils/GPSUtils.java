cc93a6e447cc9b6df3a85fe5d951d9c9232d55b1
cachil01 <cachil01@cs.ucy.ac.cy>
2020-11-27 19:02:24 +0200

    removed lib

--- clients/android-new/lib/src/main/java/cy/ac/ucy/cs/anyplace/lib/android/utils/GPSUtils.java
+++
@@ -1,9 +0,0 @@
-package cy.ac.ucy.cs.anyplace.lib.android.utils;
-
-public class GPSUtils {
-
-  //TODO: Request permissions then call from onStart and onConnected.
-
-
-  //TODO: Move all fake gps here.
-}
--- clients/android-new/lib/src/main/java/cy/ac/ucy/cs/anyplace/lib/android/utils/MapUtils.java
+++
@@ -1,8 +0,0 @@
-package cy.ac.ucy.cs.anyplace.lib.android.utils;
-
-public class MapUtils {
-
-  //TODO: addallbuildings() to google map. Pass the map. Make into object with constructor.
-
-
-}
