a44dbeb989ddf2465fb8a755ca1214c0285ca475
Nicolas Neofytou <nneof@users.noreply.github.com>
2021-01-22 10:46:51 +0200

    m

---
+++ server/database/pullDocuments.py
@@ -0,0 +1,6 @@
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/path/to/application/app/folder')
+
+import file
+print(DIR_MIGRATION)
--- server/database/pullDocuments.sh
+++ server/database/pullDocuments.sh
@@ -8,3 +8,9 @@ exit 1
 fi
 
 sudo /opt/couchbase/bin/cbexport json --c $1:$2 --u $3 --p $4 -b $5 -f lines -o $6
+
+# DIR_MIGRATION/documents.json
+# DIR_MIGRATION/collections/
+ 
+# DIR_MIGRATION/collections/buid_1.jsonList
+# DIR_MIGRATION/collections/buid_2.jsonList
