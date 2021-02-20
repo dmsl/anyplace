eb5adee6a2f8575bd3eedf9698260556fbfbd4b8
Paschalis Mpeis <paschalis.mpeis@gmail.com>
2021-02-20 02:17:22 +0200

    updated local/remote deploy watchdog

--- server/sync.sh
+++ server/sync.sh
@@ -2,3 +2,6 @@
 __dir="$(dirname "$0")"
 
 ./$__dir/push_code.sh
+
+# trigger immediate recompilation
+curl $DOMAIN:$PORT --max-time  1
---
+++ server/watchdog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOMAIN=
+PORT=
+./sync.sh
+
+watchmedo shell-command \
+      --patterns="*.scala;*.css;*.js;*.vue;" \
+      --recursive \
+      --command='./sync.sh' \
+      .
