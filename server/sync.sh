7fb4595a8577745d731864dd69c3abc98fba9bd3
Nicolas Neofytou <nneof@users.noreply.github.com>
2021-03-08 17:36:40 +0200

    watchdog scripts

--- server/sync.sh
+++ server/sync.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 __dir="$(dirname "$0")"
+source $__dir/config.sh
 
 ./$__dir/push_code.sh
+
+# trigger immediate recompilation
+curl $DOMAIN:$RPORT --max-time  1
--- server/watchdog.sh
+++ server/watchdog.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#watchmedo shell-command --patterns="*.scala" --recursive --command="./sync.sh"
+
+
+#--patterns="*.scala;*.css;*.js;*.vue;" \
+
 ./sync.sh
 watchmedo shell-command \
-      --patterns="*.scala;*.css;*.js;*.vue;" \
+      --patterns="*.scala;*.js;*.css;*.vue" \
       --recursive \
-      --command="./sync.sh" \
-      .
+      --command="./sync.sh"
