05490aa66b45f1e3265825ab4ec2e2eb0ea5c502
nneofy01 ap-dev <nneofy01@cs.ucy.ac.cy>
2021-06-09 11:43:47 +0300

    m

--- server/public/new2/src/app/app.module.ts
+++ server/public/new2/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
